91fb1db0a2df5a7b4e0a01afea167737380aa2c8
Noam Yefet <snyefet@csf.technion.ac.il>
2017-12-28 00:28:25 +0200

    adding opening book

---
+++ opening_book.py
@@ -0,0 +1,55 @@
+import operator
+
+
+class OpeningBook:
+
+    FILE_PATH = "data/book.gam"
+    MOVES = 10
+    MOVE_LENGHT = 3
+    TOP = 70
+
+    def __init__(self):
+        self.openings = {}
+        self.openingsMoves = {}
+        self.openings = self.__analyzeFile()
+        self.openingsMoves = self.__buildOpeningMove(self.openings)
+
+    def __fixChar(self, ch):
+        if (ch >= 'a') and (ch <= 'h'):
+            ch = chr(ord(ch) - ord('a') + ord('0'))
+        elif (ch >= '1') and (ch <= '9'):
+            ch = chr(ord(ch) - 1)
+        else:
+            ch = ''
+
+        return ch
+
+    def __analyzeFile(self):
+        with open(OpeningBook.FILE_PATH, "r") as file:
+            for line in file:
+                opening = line[:OpeningBook.MOVES * OpeningBook.MOVE_LENGHT]
+                #format moves:
+                opening = ''.join(map(self.__fixChar, opening))
+                if opening in self.openings:
+                    self.openings[opening] += 1
+                else:
+                    self.openings[opening] = 1
+
+            topOpening = sorted(self.openings.items(), key=operator.itemgetter(1))
+            print(topOpening)
+            topOpening = topOpening[-OpeningBook.TOP:]
+            topOpening = [ item[0] for item in topOpening]
+
+        return topOpening
+
+    def __buildOpeningMove(self, opening):
+        result = {}
+        for o in opening:
+            for i in range(2,OpeningBook.MOVES * (OpeningBook.MOVE_LENGHT-1) - 2,2):
+                index = o[:i]
+                result[index] = [int(o[i]), int(o[i+1])]
+
+        return result
+
+if __name__ == '__main__':
+    OpeningBook()
